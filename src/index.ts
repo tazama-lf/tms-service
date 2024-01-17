945b537cf5780d37204f0864fdefdb199224b677
rtkay123 <rodney.kanjala@sybrin.com>
2024-01-17 11:05:11 +0200

    fix: pseudonyms cert path

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,7 @@ const databaseManagerConfig = {
     url: configuration.transactionHistoryURL,
   },
   pseudonyms: {
-    certPath: configuration.transactionHistoryCertPath,
+    certPath: configuration.pseudonymsCertPath,
     databaseName: configuration.graphDb,
     user: configuration.pseudonymsUser,
     password: configuration.pseudonymsPassword,
