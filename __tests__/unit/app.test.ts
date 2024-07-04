a555412e267f83eb6173c9f91faaf020bddbd242
Len Bekker <len.bekker@sybrin.com>
2024-07-04 15:04:29 +0200

    tests: refactor update and bad test

--- __tests__/unit/app.test.ts
+++ __tests__/unit/app.test.ts
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Pacs002, Pacs008, Pain001, Pain013 } from '@frmscoe/frms-coe-lib/lib/interfaces';
-import { cacheDatabaseClient, databaseManager, dbInit, runServer, server } from '../../src/index';
+import { cacheDatabaseManager, dbInit, runServer, server } from '../../src/index';
 import * as LogicService from '../../src/logic.service';
 import { configuration } from '../../src/config';
 import { CacheDatabaseClientMocks, DatabaseManagerMocks } from '@frmscoe/frms-coe-lib/lib/tests/mocks/mock-transactions';
@@ -13,14 +13,13 @@ beforeAll(async () => {
 });
 
 afterAll(() => {
-  cacheDatabaseClient.quit();
-  databaseManager.quit();
+  cacheDatabaseManager.quit();
 });
 
 describe('App Controller & Logic Service', () => {
   beforeEach(() => {
-    CacheDatabaseClientMocks(cacheDatabaseClient);
-    DatabaseManagerMocks(databaseManager);
+    CacheDatabaseClientMocks(cacheDatabaseManager);
+    DatabaseManagerMocks(cacheDatabaseManager);
 
     jest.spyOn(server, 'handleResponse').mockImplementation(jest.fn());
   });
@@ -36,7 +35,7 @@ describe('App Controller & Logic Service', () => {
       const handleSpy = jest.spyOn(LogicService, 'handlePain001');
 
       await LogicService.handlePain001(request, 'pain.001.001.11');
-      expect(handleSpy).toBeCalledTimes(1);
+      expect(handleSpy).toHaveBeenCalledTimes(1);
       expect(handleSpy).toHaveReturned();
     });
 
@@ -44,7 +43,7 @@ describe('App Controller & Logic Service', () => {
       const request = Pain001Sample as Pain001;
 
       jest
-        .spyOn(cacheDatabaseClient, 'saveTransactionHistory')
+        .spyOn(cacheDatabaseManager, 'saveTransactionHistory')
         .mockImplementation((transaction: any, transactionhistorycollection: string) => {
           return new Promise((resolve, reject) => {
             throw new Error('Deliberate Error');
@@ -68,7 +67,7 @@ describe('App Controller & Logic Service', () => {
       const handleSpy = jest.spyOn(LogicService, 'handlePain013');
 
       await LogicService.handlePain013(request, 'pain.013.001.09');
-      expect(handleSpy).toBeCalledTimes(1);
+      expect(handleSpy).toHaveBeenCalledTimes(1);
       expect(handleSpy).toHaveReturned();
     });
 
@@ -76,7 +75,7 @@ describe('App Controller & Logic Service', () => {
       const request = Pain013Sample as Pain013;
 
       jest
-        .spyOn(cacheDatabaseClient, 'saveTransactionHistory')
+        .spyOn(cacheDatabaseManager, 'saveTransactionHistory')
         .mockImplementation((transaction: any, transactionhistorycollection: string) => {
           return new Promise((resolve, reject) => {
             throw new Error('Deliberate Error');
@@ -100,13 +99,13 @@ describe('App Controller & Logic Service', () => {
       const handleSpy = jest.spyOn(LogicService, 'handlePacs008');
 
       await LogicService.handlePacs008(request, 'pacs.008.001.10');
-      expect(handleSpy).toBeCalledTimes(1);
+      expect(handleSpy).toHaveBeenCalledTimes(1);
       expect(handleSpy).toHaveReturned();
     });
 
     it('should handle pacs.008, database error', async () => {
       jest
-        .spyOn(cacheDatabaseClient, 'saveTransactionHistory')
+        .spyOn(cacheDatabaseManager, 'saveTransactionHistory')
         .mockImplementation((transaction: Pain001 | Pain013 | Pacs008 | Pacs002, transactionhistorycollection: string) => {
           return new Promise((resolve, reject) => {
             throw new Error('Deliberate Error');
@@ -132,7 +131,7 @@ describe('App Controller & Logic Service', () => {
       const handleSpy = jest.spyOn(LogicService, 'handlePacs008');
 
       await LogicService.handlePacs008(request, 'pacs.008.001.10');
-      expect(handleSpy).toBeCalledTimes(1);
+      expect(handleSpy).toHaveBeenCalledTimes(1);
       expect(handleSpy).toHaveReturned();
       configuration.quoting = false;
     });
@@ -140,7 +139,7 @@ describe('App Controller & Logic Service', () => {
 
   describe('handlePacs.002', () => {
     it('should handle pacs.002', async () => {
-      jest.spyOn(cacheDatabaseClient, 'getTransactionHistoryPacs008').mockImplementation((EndToEndId: string) => {
+      jest.spyOn(cacheDatabaseManager, 'getTransactionPacs008').mockImplementation((EndToEndId: string) => {
         return Promise.resolve(
           JSON.parse(
             '[[{"TxTp":"pacs.008.001.10","FIToFICstmrCdtTrf":{"GrpHdr":{"MsgId":"cabb-32c3-4ecf-944e-654855c80c38","CreDtTm":"2023-02-03T07:17:52.216Z","NbOfTxs":1,"SttlmInf":{"SttlmMtd":"CLRG"}},"CdtTrfTxInf":{"PmtId":{"InstrId":"4ca819baa65d4a2c9e062f2055525046","EndToEndId":"701b-ae14-46fd-a2cf-88dda2875fdd"},"IntrBkSttlmAmt":{"Amt":{"Amt":31020.89,"Ccy":"USD"}},"InstdAmt":{"Amt":{"Amt":9000,"Ccy":"ZAR"}},"ChrgBr":"DEBT","ChrgsInf":{"Amt":{"Amt":307.14,"Ccy":"USD"},"Agt":{"FinInstnId":{"ClrSysMmbId":{"MmbId":"typology003"}}}},"InitgPty":{"Nm":"April Blake Grant","Id":{"PrvtId":{"DtAndPlcOfBirth":{"BirthDt":"1968-02-01","CityOfBirth":"Unknown","CtryOfBirth":"ZZ"},"Othr":[{"Id":"+01-710694778","SchmeNm":{"Prtry":"MSISDN"}}]}},"CtctDtls":{"MobNb":"+01-710694778"}},"Dbtr":{"Nm":"April Blake Grant","Id":{"PrvtId":{"DtAndPlcOfBirth":{"BirthDt":"1968-02-01","CityOfBirth":"Unknown","CtryOfBirth":"ZZ"},"Othr":[{"Id":"+01-710694778","SchmeNm":{"Prtry":"MSISDN"}}]}},"CtctDtls":{"MobNb":"+01-710694778"}},"DbtrAcct":{"Id":{"Othr":[{"Id":"+01-710694778","SchmeNm":{"Prtry":"MSISDN"}}]},"Nm":"April Grant"},"DbtrAgt":{"FinInstnId":{"ClrSysMmbId":{"MmbId":"typology003"}}},"CdtrAgt":{"FinInstnId":{"ClrSysMmbId":{"MmbId":"dfsp002"}}},"Cdtr":{"Nm":"Felicia Easton Quill","Id":{"PrvtId":{"DtAndPlcOfBirth":{"BirthDt":"1935-05-08","CityOfBirth":"Unknown","CtryOfBirth":"ZZ"},"Othr":[{"Id":"+07-197368463","SchmeNm":{"Prtry":"MSISDN"}}]}},"CtctDtls":{"MobNb":"+07-197368463"}},"CdtrAcct":{"Id":{"Othr":[{"Id":"+07-197368463","SchmeNm":{"Prtry":"MSISDN"}}]},"Nm":"Felicia Quill"},"Purp":{"Cd":"MP2P"}},"RgltryRptg":{"Dtls":{"Tp":"BALANCE OF PAYMENTS","Cd":"100"}},"RmtInf":{"Ustrd":"Payment of USD 30713.75 from April to Felicia"},"SplmtryData":{"Envlp":{"Doc":{"Xprtn":"2023-02-03T07:17:52.216Z"}}}}}]]',
@@ -157,7 +156,7 @@ describe('App Controller & Logic Service', () => {
     });
 
     it('should handle pacs.002, database error', async () => {
-      jest.spyOn(cacheDatabaseClient, 'getTransactionHistoryPacs008').mockImplementation((EndToEndId: string) => {
+      jest.spyOn(cacheDatabaseManager, 'getTransactionPacs008').mockImplementation((EndToEndId: string) => {
         return new Promise((resolve, reject) => {
           throw new Error('Deliberate Error');
         });
@@ -178,19 +177,15 @@ describe('App Controller & Logic Service', () => {
     it('should fail gracefully - rebuildCache', async () => {
       const request = Pacs002Sample as Pacs002;
 
-      jest.spyOn(databaseManager, 'getBuffer').mockRejectedValue((key: any) => {
+      jest.spyOn(cacheDatabaseManager, 'getBuffer').mockRejectedValue((key: any) => {
         return Promise.resolve('some error');
       });
 
-      jest.spyOn(databaseManager, 'getTransactionPain001').mockImplementation((key: any) => {
+      jest.spyOn(cacheDatabaseManager, 'getTransactionPacs008').mockImplementation((key: any) => {
         return Promise.resolve('');
       });
 
-      jest.spyOn(databaseManager, 'getTransactionPacs008').mockImplementation((key: any) => {
-        return Promise.resolve('');
-      });
-
-      jest.spyOn(cacheDatabaseClient, 'getTransactionHistoryPacs008').mockImplementation((key: any) => {
+      jest.spyOn(cacheDatabaseManager, 'getTransactionPacs008').mockImplementation((key: any) => {
         return Promise.resolve([
           [
             JSON.parse(
@@ -203,32 +198,29 @@ describe('App Controller & Logic Service', () => {
       const handleSpy = jest.spyOn(LogicService, 'handlePacs002');
 
       await LogicService.handlePacs002(request, 'pacs.002.001.12');
-      expect(handleSpy).toBeCalledTimes(1);
+      expect(handleSpy).toHaveBeenCalledTimes(1);
       expect(handleSpy).toHaveReturned();
 
-      jest.spyOn(databaseManager, 'getTransactionPacs008').mockImplementation((key: any) => {
+      jest.spyOn(cacheDatabaseManager, 'getTransactionPacs008').mockImplementation((key: any) => {
         return Promise.resolve([[Pacs008Sample]]);
       });
       await LogicService.handlePacs002(request, 'pacs.002.001.12');
-      expect(handleSpy).toBeCalledTimes(2);
+      expect(handleSpy).toHaveBeenCalledTimes(2);
       expect(handleSpy).toHaveReturned();
     });
-    it('should fail gracefully - rebuildCachePain001', async () => {
-      const request = Pain013Sample as Pain013;
 
-      jest.spyOn(databaseManager, 'getBuffer').mockRejectedValue((key: any) => {
+    it('should fail gracefully - rebuildCache', async () => {
+      const request = Pacs002Sample as Pacs002;
+
+      jest.spyOn(cacheDatabaseManager, 'getBuffer').mockRejectedValue((key: any) => {
         return Promise.resolve('some error');
       });
 
-      jest.spyOn(databaseManager, 'getTransactionPain001').mockImplementation((key: any) => {
-        return Promise.resolve('');
+      jest.spyOn(cacheDatabaseManager, 'getTransactionPacs008').mockImplementationOnce((key: any) => {
+        return Promise.resolve([[]]);
       });
 
-      jest.spyOn(databaseManager, 'getTransactionPacs008').mockImplementation((key: any) => {
-        return Promise.resolve('');
-      });
-
-      jest.spyOn(cacheDatabaseClient, 'getTransactionHistoryPacs008').mockImplementation((key: any) => {
+      jest.spyOn(cacheDatabaseManager, 'getTransactionPacs008').mockImplementation((key: any) => {
         return Promise.resolve([
           [
             JSON.parse(
@@ -238,11 +230,12 @@ describe('App Controller & Logic Service', () => {
         ]);
       });
 
-      jest.spyOn(databaseManager, 'getTransactionPain001').mockImplementation((key: any) => {
-        return Promise.resolve([[Pain001Sample]]);
-      });
+      const handleSpy = jest.spyOn(LogicService, 'handlePacs002');
 
-      await LogicService.handlePain013(request, 'pain.013.001.09');
+      await LogicService.handlePacs002(request, 'pacs.002.001.12');
+
+      expect(handleSpy).toHaveBeenCalledTimes(1);
+      expect(handleSpy).toHaveReturned();
     });
   });
 });
