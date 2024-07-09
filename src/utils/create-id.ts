82f5733fcee0d6de4440af20ed6341499cdab5ee
rtkay123 <rodney.kanjala@sybrin.com>
2024-07-09 14:19:59 +0200

    docs: fn signature for helper functions

--- src/utils/create-id.ts
+++ src/utils/create-id.ts
@@ -1,7 +1,28 @@
+/**
+ * Concatenates an `id` and `prtry` values
+ *
+ * @remarks
+ * This function returns a type to be used by entity IDs
+ * @param id - `id` value
+ * @param prtry - `prtry` value
+ * @returns A string joining the two input parameters in order
+ *
+ */
 export function createId(id: string, prtry: string): string {
   return `${id}${prtry}`;
 }
 
+/**
+ * Concatenates an `id`, `prtry` and `mmbId` values
+ *
+ * @remarks
+ * This function returns a type to be used by account IDs
+ * @param id - `id` value
+ * @param prtry - `prtry` value
+ * @param mmbId - `mmbId` value
+ * @returns A string joining the three input parameters in order
+ *
+ */
 export function createAccountId(id: string, prtry: string, mmbId: string): string {
   return `${createId(id, prtry)}${mmbId}`;
 }
