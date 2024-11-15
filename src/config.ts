dc44c0534a6f2e838ce14f9c3e6ea541944c572e
Cebolenkosi Shezi <cshezi@sybrin.co.za>
2024-11-15 16:14:55 +0200

    refactor: remove collection based environment variables

--- src/config.ts
+++ src/config.ts
@@ -17,32 +17,12 @@ export const additionalEnvironmentVariables: AdditionalConfig[] = [
     name: 'AUTHENTICATED',
     type: 'boolean',
   },
-  {
-    name: 'TRANSACTION_HISTORY_PAIN001_COLLECTION',
-    type: 'string',
-  },
-  {
-    name: 'TRANSACTION_HISTORY_PAIN013_COLLECTION',
-    type: 'string',
-  },
-  {
-    name: 'TRANSACTION_HISTORY_PACS008_COLLECTION',
-    type: 'string',
-  },
-  {
-    name: 'TRANSACTION_HISTORY_PACS002_COLLECTION',
-    type: 'string',
-  },
 ];
 
 export interface ExtendedConfig {
   PORT: number;
   QUOTING: boolean;
   AUTHENTICATED: boolean;
-  TRANSACTION_HISTORY_PAIN001_COLLECTION: string;
-  TRANSACTION_HISTORY_PAIN013_COLLECTION: string;
-  TRANSACTION_HISTORY_PACS008_COLLECTION: string;
-  TRANSACTION_HISTORY_PACS002_COLLECTION: string;
 }
 
 type Databases = Required<Pick<ManagerConfig, 'transactionHistory' | 'pseudonyms' | 'redisConfig'>>;
