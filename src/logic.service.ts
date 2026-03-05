a8a9cfa8d0e4a87a16fcd8048fc3eba66b78b32e
abdul-rahim-psl <abdul.rahim@paysyslabs.com>
2026-03-05 10:38:39 +0500

    feat: updated addAccount wrapper and all msg handlers

--- src/logic.service.ts
+++ src/logic.service.ts
@@ -183,8 +183,8 @@ export const handlePain001 = async (transaction: Pain001): Promise<void> => {
   try {
     await Promise.all([
       cacheDatabaseManager.saveTransactionHistory(transaction),
-      cacheDatabaseManager.addAccount(debtorAcctId, TenantId),
-      cacheDatabaseManager.addAccount(creditorAcctId, TenantId),
+      cacheDatabaseManager.addAccount(debtorAcctId, TenantId, CreDtTm),
+      cacheDatabaseManager.addAccount(creditorAcctId, TenantId, CreDtTm),
       cacheDatabaseManager.addEntity(creditorId, TenantId, CreDtTm),
       cacheDatabaseManager.addEntity(debtorId, TenantId, CreDtTm),
     ]);
@@ -264,8 +264,8 @@ export const handlePain013 = async (transaction: Pain013): Promise<void> => {
   try {
     await Promise.all([
       cacheDatabaseManager.saveTransactionHistory(transaction),
-      cacheDatabaseManager.addAccount(debtorAcctId, TenantId),
-      cacheDatabaseManager.addAccount(creditorAcctId, TenantId),
+      cacheDatabaseManager.addAccount(debtorAcctId, TenantId, CreDtTm),
+      cacheDatabaseManager.addAccount(creditorAcctId, TenantId, CreDtTm),
     ]);
 
     await cacheDatabaseManager.saveTransactionDetails(transactionDetails);
@@ -319,7 +319,10 @@ export const handlePacs008 = async (transaction: Pacs008): Promise<void> => {
     TenantId,
   };
 
-  const pendingPromises = [cacheDatabaseManager.addAccount(dbtrAcctId, TenantId), cacheDatabaseManager.addAccount(cdtrAcctId, TenantId)];
+  const pendingPromises = [
+    cacheDatabaseManager.addAccount(dbtrAcctId, TenantId, creDtTm),
+    cacheDatabaseManager.addAccount(cdtrAcctId, TenantId, creDtTm),
+  ];
 
   const dataCache: DataCache = {
     cdtrId,
